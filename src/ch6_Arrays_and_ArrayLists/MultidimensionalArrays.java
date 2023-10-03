cbd4be83d4a738e30a9b1f4a0b3187e8adc3a7f8
ÖMER GÜNDOĞDU <omergundogdu75@gmail.com>
2023-10-04 00:12:25 +0300

    update

--- src/ch6_Arrays_and_ArrayLists/MultidimensionalArrays.java
+++ src/ch6_Arrays_and_ArrayLists/MultidimensionalArrays.java
@@ -11,6 +11,35 @@ public class MultidimensionalArrays {
         int[][] e = new int[2][]; // create 2 rows
         e[0] = new int[5]; // create 5 columns for row 0
         e[1] = new int[3]; // create 3 columns for row 1
-        //6.10.4
+
+        int[][] array1 = {{1, 2, 3}, {4, 5, 6}};
+        int[][] array2 = {{1, 2}, {3}, {4, 5, 6}};
+        System.out.println("Values in array1 by row are");
+        outputArray(array1); // displays array1 by row
+        System.out.printf("%nValues in array2 by row are%n");
+        outputArray(array2); // displays array2 by row
+
+    }
+
+    // output rows and columns of a two-dimensional array
+    public static void outputArray(int[][] array) {
+        // loop through array's rows
+        for (int row = 0; row < array.length; row++) {
+            // loop through columns of current row
+            for (int column = 0; column < array[row].length; column++) {
+                System.out.printf("%d ", array[row][column]);
+            }
+
+            System.out.println();
+        }
     }
 }
+/*
+*
+* int total = 0;
+for (int row = 0; row < a.length; row++) {
+ for (int column = 0; column < a[row].length; column++) {
+ total += a[row][column];
+ }
+}
+* */
