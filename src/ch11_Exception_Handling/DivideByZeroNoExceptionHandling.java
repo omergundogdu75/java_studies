1d2b8d92df8204ff031d6d3d3d4017c39f838f4c
ÖMER GÜNDOĞDU <omergundogdu75@gmail.com>
2023-10-11 11:53:49 +0300

    Exception konusu işlendi

---
+++ src/ch11_Exception_Handling/DivideByZeroNoExceptionHandling.java
@@ -0,0 +1,23 @@
+package ch11_Exception_Handling;
+
+import java.util.Scanner;
+
+public class DivideByZeroNoExceptionHandling {
+    // demonstrates throwing an exception when a divide-by-zero occurs
+    public static int quotient(int numerator, int denominator) {
+        return numerator / denominator; // possible division by zero
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter an integer numerator: ");
+        int numerator = scanner.nextInt();
+        System.out.print("Please enter an integer denominator: ");
+        int denominator = scanner.nextInt();
+
+        int result = quotient(numerator, denominator);
+        System.out.printf(
+                "%nResult: %d / %d = %d%n", numerator, denominator, result);
+    }
+}
---
+++ src/ch11_Exception_Handling/UsingExceptions.java
@@ -0,0 +1,40 @@
+package ch11_Exception_Handling;
+
+public class UsingExceptions {
+
+    public static void main(String[] args) {
+        try {
+            throwException();
+        } catch (Exception exception) { // exception thrown by throwException
+            System.err.println("Exception handled in main");
+        }
+        doesNotThrowException();
+    }
+
+    // demonstrate try...catch...finally
+    public static void throwException() throws Exception {
+        try { // throw an exception and immediately catch it
+            System.out.println("Method throwException");
+            throw new Exception(); // generate exception
+        } catch (Exception exception) { // catch exception thrown in try
+            System.err.println(
+                    "Exception handled in method throwException");
+            throw new Exception(); // generate exception
+            // code here would not be reached; would cause compilation errors
+        } finally { // executes regardless of what occurs in try...catch
+            System.err.println("Finally executed in throwException");
+        }
+    }
+
+    public static void doesNotThrowException() {
+        try { // try block does not throw an exception
+            System.out.println("Method doesNotThrowException");
+        } catch (Exception exception) { // does not execute
+            System.err.println(exception);
+        }finally { // executes regardless of what occurs in try...catch
+            System.err.println("Finally executed in doesNotThrowException");
+        }
+        System.out.println("End of method doesNotThrowException");
+    }
+
+}
